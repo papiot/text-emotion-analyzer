7b2d4b0f14cc12ad6eab7cbb5a14d7ba07dd8399
Catalin Avram <catalin@fairfleeet360.com>
2018-03-04 20:58:41 +0100

    Call MS Cognitive Services with real data

--- dist/app.js
+++ dist/app.js
@@ -8,7 +8,45 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // Main function
 var main = function main() {
-    console.log("Hello World...");
+    var positiveText = {
+        "documents": [{
+            "language": "en",
+            "id": "1234",
+            "text": "It is such a beautiful day!!"
+        }]
+    };
+
+    var negativeText = {
+        "documents": [{
+            "language": "en",
+            "id": "1235",
+            "text": "This is such a lousy computer."
+        }]
+    };
+    callServer(positiveText);
+    callServer(negativeText);
+};
+
+var callServer = function callServer(withText) {
+    console.log("For text: " + withText.documents[0].text);
+
+    // You need MS Cognitive services API key for this to work
+    var authOptions = {
+        method: 'POST',
+        url: 'https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+        data: withText,
+        headers: {
+            'Ocp-Apim-Subscription-Key': process.env.MS_COGNITIVE_TEXT_ANALYZER,
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        json: true
+    };
+    (0, _axios2.default)(authOptions).then(function (response) {
+        console.log(response.data);
+        console.log(response.status);
+    }).catch(function (error) {
+        console.log(error);
+    });
 };
 
 main();
--- src/app.js
+++ src/app.js
@@ -3,7 +3,51 @@ import axios from 'axios';
 
 // Main function
 const main = () => {
-    console.log("Hello World...")
+    const positiveText = {
+        "documents": [
+            {
+                "language": "en",
+                "id": "1234",
+                "text": "It is such a beautiful day!!"
+            }
+        ]
+    }
+
+    const negativeText = {
+        "documents": [
+            {
+                "language": "en",
+                "id": "1235",
+                "text": "This is such a lousy computer."
+            }
+        ]
+    }
+    callServer(positiveText);
+    callServer(negativeText);
+}
+
+const callServer = (withText) => {
+    console.log("For text: " + withText.documents[0].text);
+
+    // You need MS Cognitive services API key for this to work
+    const authOptions = {
+            method: 'POST',
+            url: 'https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+            data: withText,
+            headers: {
+                'Ocp-Apim-Subscription-Key': process.env.MS_COGNITIVE_TEXT_ANALYZER,
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            json: true
+        };
+        axios(authOptions)
+            .then((response) => {
+                console.log(response.data);
+                console.log(response.status);
+            })
+            .catch((error) => {
+                console.log(error)
+            })
 }
 
 main();
