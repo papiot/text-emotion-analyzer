040619108226f7312fdaa51d4c559cc758fc05a6
Catalin Avram <papiot@gmail.com>
2018-05-22 19:38:39 +0200

    Add Dockerfile

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ const startServer = () => {
 
     app.get('/', (req, res) => res.send('Hello World!'))
 
-    app.listen(3000, () => console.log('Server started on port 3000'))
+    app.listen(4000, () => console.log('Server started on port 4000'))
 
     // Endpoint used by the frontend to analyze the text
     app.post('/analyze-sentiment', (req,res) => {
