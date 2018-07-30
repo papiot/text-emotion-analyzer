9fe4320de70126069b142f788c42f00e9f60b365
Catalin Avram <papiot@gmail.com>
2018-07-30 20:14:24 +0200

    Add support for Dutch translation

--- src/app.js
+++ src/app.js
@@ -40,8 +40,93 @@ const startServer = () => {
         );
     })
 
+    // Test translation
+    app.get('/test-translation', (req, res) => {
+        console.log("Testing Translation...");
+
+        let LanguageTranslatorV3 = require('watson-developer-cloud/language-translator/v3');
+
+        let languageTranslator = new LanguageTranslatorV3({
+            version: '2018-05-01',
+            iam_apikey: process.env.IBM_WATSON_TRANSLATION_API_KEY,
+            url: "https://gateway-fra.watsonplatform.net/language-translator/api"
+        });
+
+        let parameters = {
+            text: 'Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may. ',
+            model_id: 'en-nl'
+        };
+
+        languageTranslator.translate(parameters, (err, response) => {
+            if (err) {
+                console.log("Something went wrong with translation: ");
+                console.log(err)
+            } else {
+                // console.log(JSON.stringify(response, null, 2));
+                const translation = JSON.stringify(response)
+
+                console.log(response.translations[0].translation)
+            }
+        })
+    })
+
     app.post('/analyze-watson', (req, res) => {
         const phraze = req.body.my_text_to_analyze;
+        const lng = req.body.language;
+
+        if (lng !== 'en') {
+            let LanguageTranslatorV3 = require('watson-developer-cloud/language-translator/v3');
+
+            let languageTranslator = new LanguageTranslatorV3({
+                version: '2018-05-01',
+                iam_apikey: process.env.IBM_WATSON_TRANSLATION_API_KEY,
+                url: "https://gateway-fra.watsonplatform.net/language-translator/api"
+            });
+
+            let parameters = {
+                text: phraze,
+                model_id: 'nl-en'
+            };
+
+            languageTranslator.translate(parameters, (err, response) => {
+                if (err) {
+                    console.log("Something went wrong with translation: ");
+                    console.log(err)
+                } else {
+                    const translatedText = response.translations[0].translation
+                    console.log("Translated text: ")
+                    console.log(translatedText)
+
+                    let ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+                    let tone_analyzer = new ToneAnalyzerV3({
+                        username: process.env.IBM_WATSON_TONE_ANALYZER_USERNAME,
+                        password: process.env.IBM_WATSON_TONE_ANALYZER_PASSWORD,
+                        version_date: '2017-09-21'
+                    });
+
+                    var params = {
+                        'tone_input': {
+                                        "text": translatedText
+                                    },
+                        'content_type': 'application/json'
+                    };
+
+                    tone_analyzer.tone(params, (error, response) => {
+                        if (error)
+                            console.log('error:', error);
+                        else
+                            console.log(JSON.stringify(response, null, 2));
+                            response.english_translation = translatedText
+                            res.write(JSON.stringify(response, null, 2));
+                            res.end()
+                        }
+                    );
+                }
+            })
+
+            return
+        }
 
         let ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
